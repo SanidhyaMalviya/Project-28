1d2476ac5622e854d33a69695c1c75f3c0b5be07
SanidhyaMalviya <72500621+SanidhyaMalviya@users.noreply.github.com>
2020-11-15 17:03:21 +0530

    Update Tree.js

--- Tree.js
+++ Tree.js
@@ -10,6 +10,6 @@ class Tree{
     display(){ 
         //var pos = this.body.position
         imageMode(CENTER)
-        image(this.image,pos.x,pos.y,this.width,this.height)
+        image(this.image,850,346,this.width,this.height);
     }
 }
